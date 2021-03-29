68292740c301bcff07dae0cacd742888264f6ec4
lucio rendine <luciorendine69@gmail.com>
2021-03-30 01:07:34 +0200

    push

---
+++ MyProject/database/migrations/2021_03_29_220150_create_author_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+
+            $table->text('bio');
+            $table->string('pic', 2048);
+            $table->string('website', 2048);
+            $table->timestamps();
+
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author_details');
+    }
+}
